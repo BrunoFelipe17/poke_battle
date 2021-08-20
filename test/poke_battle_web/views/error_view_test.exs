87472f024bca351ef45f19298fba45102f7e09b3
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-20 15:33:34 -0300

    Project created

---
+++ test/poke_battle_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule PokeBattleWeb.ErrorViewTest do
+  use PokeBattleWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(PokeBattleWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(PokeBattleWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
