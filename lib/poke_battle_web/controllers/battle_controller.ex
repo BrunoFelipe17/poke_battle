262f39138c2c67e4a679132c1584d6037efe28d8
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-21 10:03:27 -0300

    Showing the battle with the given id

--- lib/poke_battle_web/controllers/battle_controller.ex
+++ lib/poke_battle_web/controllers/battle_controller.ex
@@ -3,11 +3,17 @@ defmodule PokeBattleWeb.BattleController do
 
   def index(conn, _) do
     with {:ok, battles} <- PokeBattle.fetch_all_battles() do
-      IO.inspect(battles)
-
       conn
       |> put_status(:ok)
       |> render("index.json", battles: battles)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, %PokeBattle.Battle{} = battle} <- PokeBattle.fetch_by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", battle: battle)
     end
   end
 end
--- lib/poke_battle_web/views/battle_view.ex
+++ lib/poke_battle_web/views/battle_view.ex
@@ -2,4 +2,6 @@ defmodule PokeBattleWeb.BattleView do
   use PokeBattleWeb, :view
 
   def render("index.json", %{battles: battles}), do: %{"battles" => battles}
+
+  def render("show.json", %{battle: battle}), do: battle
 end
