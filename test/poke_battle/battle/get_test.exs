4c51f0653ea45799d087cd78932d2b21912471ed
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-29 12:23:05 -0300

    Adding errors test in get module

--- test/poke_battle/battle/get_test.exs
+++ test/poke_battle/battle/get_test.exs
@@ -3,23 +3,6 @@ defmodule PokeBattle.Battle.GetTest do
 
   alias PokeBattle.{Battle, Battle.Get, Repo}
 
-  describe "all/1" do
-    setup do
-      {_ok, battle1} = Repo.insert(%Battle{pokemon_one: "pikachu", pokemon_two: "charmander"})
-      {_ok, battle2} = Repo.insert(%Battle{pokemon_one: "charizard", pokemon_two: "blastoise"})
-      %{battles: [battle1, battle2]}
-    end
-
-    test "returns all battles", %{battles: battles} do
-      battle1 = Enum.at(battles, 0)
-      battle2 = Enum.at(battles, 1)
-
-      # assert "a" == "b"
-      assert {:ok, battle1} == Get.by_id(battle1.id)
-      assert {:ok, battle2} == Get.by_id(battle2.id)
-    end
-  end
-
   describe "by_id/1" do
     setup do
       {_ok, battle1} = Repo.insert(%Battle{pokemon_one: "pikachu", pokemon_two: "charmander"})
@@ -28,6 +11,28 @@ defmodule PokeBattle.Battle.GetTest do
 
     test "return a battle with the given id", %{battle: battle} do
       assert {:ok, %Battle{}} = Get.by_id(battle.id)
+    end
+
+    test "return an error when a battle is not found" do
+      random_uuid = "895218c6-8461-47fd-b346-3e1d43641b6f"
+
+      response = Get.by_id(random_uuid)
+
+      expected_response =
+        {:error, %PokeBattle.Error{result: "Battle not found", status: :not_found}}
+
+      assert response == expected_response
+    end
+
+    test "return an error when the uuid is invalid" do
+      invalid_uuid = "123"
+
+      response = Get.by_id(invalid_uuid)
+
+      expected_response =
+        {:error, %PokeBattle.Error{result: "Invalid UUID", status: :bad_request}}
+
+      assert response == expected_response
     end
   end
 end
